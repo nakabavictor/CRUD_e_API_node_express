ac6a67402ccfb2d0814739e5b0c6e63da0b59703
Victor Nakabayashi <127894385+nakabavictor@users.noreply.github.com>
2025-08-05 19:24:19 -0300

    tentando usar schema e validation antes de ir para o banco de dados

---
+++ src/middlewares/validationMiddleware.js
@@ -0,0 +1,12 @@
+const validate = (schema) => (req, res, next)=>{
+    try{
+        schema.parse(req.body)
+        next()
+    }catch(e){
+        res.status(400).json({error: e.errors})
+    }
+}
+
+export{
+    validate
+}
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userController from '../controller/userController.js'
+import { validate } from '../middlewares/validationMiddleware.js';
+import { userSchema } from '../schema/userSchema.js';
 
 const router = Router()
 
-router.post('/users', userController.creatUserController)
+router.post('/users', validate(userSchema),  userController.creatUserController)
 
 export default router
