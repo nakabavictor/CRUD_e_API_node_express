c5dbdc758833e9be0817fa8f727869dd87f46989
Victor Nakabayashi <127894385+nakabavictor@users.noreply.github.com>
2025-08-05 18:42:15 -0300

    utilizando bcypt para encriptação da senha

--- src/repositories/userRepositories.js
+++ src/repositories/userRepositories.js
@@ -24,7 +24,7 @@ function createUserReposiory(newUser){
 function findUserByEmailRepository(email){
     return new Promise((resolve, reject) =>{
         db.get(`
-            SELECT id, username, email. avatar FROM users WHERE email = ?
+            SELECT id, username, email, avatar FROM users WHERE email = ?
         `, [email], (err, row) =>{
             if(err){
                 reject(err)
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,13 @@
 import userRepositories from "../repositories/userRepositories.js";
+import bcrypt from 'bcrypt'
 
 async function createUserService(newUser){
     const foundUser = await userRepositories.findUserByEmailRepository(newUser.email)
     if(foundUser) throw new Error("Usuario ja existe");
     
-    const user = await userRepositories.createUserReposiory(newUser)
+const passHash =await bcrypt.hash(newUser.password, 10)
+    const user = await userRepositories.createUserReposiory({...newUser, password: passHash})
+    if(!user) throw new Error("Erro ao criar usuario")
     return user;
 }
 
