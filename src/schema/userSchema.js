900e6e6496989aaab07f6468c9b3317e543c8c8b
Victor Nakabayashi <127894385+nakabavictor@users.noreply.github.com>
2025-08-05 19:33:49 -0300

    erro em alguma validacao

--- src/schema/userSchema.js
+++ src/schema/userSchema.js
@@ -1,7 +1,7 @@
 import {z} from 'zod'
 
 const userSchema = z.object({
-    username: z.string().min(5, {message: "O username deve ter no minimo 5 caracteres"}).max(20, { message: "O username deve ter no máximo 20 caracteres." }).regex(/^[a-zA-Z0-9_\-]+$/, { message: "O username pode conter apenas letras, números, underscores e hífens." }),
+    username: z.string().min(5, {message: "O username deve ter no minimo 5 caracteres"}).max(20, { message: "O username deve ter no máximo 20 caracteres." }),
     email: z.email().transform(val => val.trim().toLowerCase()),
     password: z.string().min(8, {message: "A senha deve conter pelo menos 8 caracteres."}),
     avatar: z.url('URL invalida.').optional(),
